1c1f3d0c544ac3f9e3c503dbf338d471fa960d07
Josh Radack <josh.radack@gmail.com>
2023-03-06 14:14:12 -0500

    Created data model for events, ran migration

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Event
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("name", "date", "venue")
--- events/models.py
+++ events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    # Required entries
+    name = models.CharField(max_length=200)
+    date = models.DateField()
+    venue = models.CharField(max_length=100)
+    # Optional entries
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    door_time = models.DateTimeField()
+    event_time = models.DateTimeField()
+    age_restriction = models.CharField(max_length=50)
+    tagline = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    event_link = models.URLField()
+    stage = models.CharField(max_length=100)
+    sold_out = models.BooleanField(default=False)
+    thumbnail_image = models.URLField()
+    # Backend fields
+    pinned = models.BooleanField(default=False)
+    hidden = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.date} @ {self.venue})"
